3b3ef666bca97f538a512d56d6554ca49e9b6375
Björn Davidsson <bjrn.davidsson@sonyericsson.com>
2010-12-06 14:18:53 +0100

    Performance: Activity manager perf unnecessary recalc of oom_adj
    If an activity has bound service content providers,
    updateLruProcessInternalLocked will be called recursively with
    the oomAdj flag set, resulting in several recalculations of oomAdj
    with unchanged data. Doing it at the end of the top level call to
    updateLruProcessInternalLocked is sufficient.
    
    Change-Id: I95e27011e1d3519f256a9bd756cbb18d43e8db29

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -1679,7 +1679,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                 if (cr.binding != null && cr.binding.service != null
                         && cr.binding.service.app != null
                         && cr.binding.service.app.lruSeq != mLruSeq) {
-                    updateLruProcessInternalLocked(cr.binding.service.app, oomAdj,
+                    updateLruProcessInternalLocked(cr.binding.service.app, false,
                             updateActivityTime, i+1);
                 }
             }
@@ -1687,7 +1687,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         if (app.conProviders.size() > 0) {
             for (ContentProviderRecord cpr : app.conProviders.keySet()) {
                 if (cpr.app != null && cpr.app.lruSeq != mLruSeq) {
-                    updateLruProcessInternalLocked(cpr.app, oomAdj,
+                    updateLruProcessInternalLocked(cpr.app, false,
                             updateActivityTime, i+1);
                 }
             }
