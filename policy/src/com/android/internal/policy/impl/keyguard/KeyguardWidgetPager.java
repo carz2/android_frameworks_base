8d1f694d67b31136ef5fc23aa60dc4e6385f2440
carz2 <carz8500@gmail.com>
2012-12-11 17:48:08 -0700

    get rid of initial side widget hints

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
@@ -566,8 +566,7 @@ public class KeyguardWidgetPager extends PagedView implements PagedView.PageSwit
         for (int i = 0; i < count; i++) {
             KeyguardWidgetFrame child = getWidgetPageAt(i);
             if (i != mCurrentPage) {
-                child.fadeFrame(this, true, KeyguardWidgetFrame.OUTLINE_ALPHA_MULTIPLIER,
-                        CHILDREN_OUTLINE_FADE_IN_DURATION);
+                child.setBackgroundAlpha(0f);
                 child.setContentAlpha(0f);
             } else {
                 child.setBackgroundAlpha(0f);
