1ae682b89badd20dfbb36d82c8b48c247f5758df
Danesh Mondegarian <daneshm90@gmail.com>
2013-01-07 12:46:46 -0500

    Launch music player on headset connect
    Launches the default music player when a headset is connected
    
    Change-Id: Ib97077191c52b14e552265456312162b6ad3fdcd
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2365,6 +2365,12 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Whether or not to launch default music player when headset is connected
+         * @hide
+         */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+        /**
          * Whether to enable quiet hours.
          * @hide
          */
